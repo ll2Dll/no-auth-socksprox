009c9ba660895588cdf22d29d4db863d2fa7d1bf
ll2Dll <76976665+ll2Dll@users.noreply.github.com>
2021-01-04 23:35:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt update
 apt upgrade -y
 apt install dante-server
 cp /etc/danted.conf /etc/danted.conf-original
-git clone https://github.com/ll2Dll/no-auth-socksprox.git
+# git clone https://github.com/ll2Dll/no-auth-socksprox.git
 mv no-auth-socksprox/danted.conf /etc/
 rm -R no-auth-socksprox
 systemctl restart danted
